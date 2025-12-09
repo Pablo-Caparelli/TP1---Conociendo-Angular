28b7e07bbf9b7399bc99f16da22f33bfa28ab36c
pablo-caparelli <pablocaparelli@gmail.com>
2025-12-09 10:08:49 -0300

    creación de nuevo componente: información, agregado de ruta de navegación, estilos, descripción de la función de cada componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Productos } from './productos/productos';
+import { Informacion } from './informacion/informacion';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'productos', component: Productos },
+  { path: 'informacion', component: Informacion },
 ];
